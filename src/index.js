ca88a148e06f5fc1ff17775ff980b0455ea11ee5
Nat <ntaktach@gmail.com>
2021-07-29 23:29:56 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux'; /* code change */
 import shoppingListItemReducer from './reducers/shoppingListItemReducer';
 import App from './App';
 import './index.css';
@@ -11,6 +12,13 @@ const store = createStore(
 );
 
 ReactDOM.render(
-  <App store={store}/>,
+  <Provider store={store}>
+    <App />
+  </Provider>, /* code change */
   document.getElementById('root')
 );
+
+//provider is redux compontent that wraps app 
+//alerts redux app when change in state and rerenders react 
+//pass store to provider as prop so its avail to other components 
+//react-redux requires to specify which changes to store state should prompt redrender (this is done via connect)
